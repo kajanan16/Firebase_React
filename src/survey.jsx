76e381b16cf824d8e3c484c9693b42e3ecf300b0
Kajanan1995 <kajanan16@gmail.com>
2019-09-11 12:11:02 +0530

    Updates

--- src/survey.jsx
+++ src/survey.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import firebase from 'firebase/app';
+import firebase from "firebase/app";
 var uuid = require("uuid");
 
 var firebaseConfig = {
@@ -27,15 +27,22 @@ export default class Survey extends Component {
       },
       isSubmitted: false
     };
+    this.mySubmit = this.mySubmit.bind(this);
   }
   mySubmit(event) {
     var studentName = this.refs.name.value;
-    this.setState({
-      studentName: studentName
-    },function(){ console.log(this.state);});
-   
-
-}
+    this.setState(
+      {
+        studentName: studentName
+      },
+      function() {
+        console.log(this.state);
+      }
+    );
+  }
+  answerSubmit() {
+    //
+  }
 
   render() {
     var studentName;
@@ -55,7 +62,96 @@ export default class Survey extends Component {
           </form>
         </div>
       );
+      questions = "";
+    } else if (
+      this.state.studentName != "" &&
+      this.state.isSubmitted == false
+    ) {
+      studentName = <h1>Hey there,{this.state.studentName}</h1>;
+      questions = (
+        <div>
+          <h2>Here are some questions :</h2>
+          <form>
+            <div className="card">
+              <label>What kind of course you like most :</label>
+              <p></p>
+              <input
+                type="radio"
+                name="answer1"
+                value="Engineering"
+                onChange={this.answerSubmit}
+              />
+              Engineering
+              <input
+                type="radio"
+                name="answer1"
+                value="Technoglogy"
+                onChange={this.answerSubmit}
+              />
+              Technoglogy
+              <input
+                type="radio"
+                name="answer1"
+                value="Design"
+                onChange={this.answerSubmit}
+              />
+              Design
+            </div>
+            <div className="card">
+              <label>What kind of Leaning Method You Prefer :</label>
+              <p></p>
+              <input
+                type="radio"
+                name="answer2"
+                value="Online"
+                onChange={this.answerSubmit}
+              />
+              Online
+              <input
+                type="radio"
+                name="answer2"
+                value="ClassRoom"
+                onChange={this.answerSubmit}
+              />
+              ClassRoom
+              <input
+                type="radio"
+                name="answer2"
+                value="Industy Base"
+                onChange={this.answerSubmit}
+              />
+              Industy Base
+            </div>
+            <div className="card">
+              <label>You are :</label>
+              <p></p>
+              <input
+                type="radio"
+                name="answer3"
+                value="Student"
+                onChange={this.answerSubmit}
+              />
+              Student
+              <input
+                type="radio"
+                name="answer3"
+                value="Employee"
+                onChange={this.answerSubmit}
+              />
+              Employee
+              <input
+                type="radio"
+                name="answer3"
+                value="Others"
+                onChange={this.answerSubmit}
+              />
+              Others
+            </div>
+          </form>
+        </div>
+      );
     }
+
     return (
       <div>
         {studentName}
